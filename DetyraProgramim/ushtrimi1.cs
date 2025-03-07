abca439719a2a98bb866ec78f511aefc25d838ef
aurelvogli <avogli5@uet.edu.al>
2025-03-07 18:08:07 +0100

    usht1

---
+++ DetyraProgramim/ushtrimi1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetyraProgramim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vendos nje numer: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
